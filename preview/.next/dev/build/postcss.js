2af7766553da09066af9059c0ec682e10b570e55
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2026-05-12 10:32:33 +0600

    remove preview

--- preview/.next/dev/build/postcss.js
+++
@@ -1,6 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("postcss.js")
-R.c("chunks/[turbopack-node]_transforms_postcss_ts_0a3.cg2._.js")
-R.c("chunks/[root-of-the-server]__0uqmscz._.js")
-R.m("[turbopack-node]/child_process/globals.ts [postcss] (ecmascript)")
-R.m("[turbopack-node]/child_process/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Documents/Landing Page Builder/neocomerz-landing-page-builder/Package/neocomerz-ui/preview/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/child_process/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
-module.exports=R.m("[turbopack-node]/child_process/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Documents/Landing Page Builder/neocomerz-landing-page-builder/Package/neocomerz-ui/preview/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/child_process/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
